0bc386155bd3769edef1c7ae008c3804613ece7b
Kacper Rzymkiewicz <kacper9800@gmail.com>
2020-01-06 15:25:22 +0100

    #4 New components, sidenav

--- src/main/sop-web/src/app/app.module.ts
+++ src/main/sop-web/src/app/app.module.ts
@@ -11,8 +11,15 @@ import { StatisticsComponent } from './statistics/statistics.component';
 import { FormsComponent } from './forms/forms.component';
 import { LogoutComponent } from './logout/logout.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatToolbarModule } from '@angular/material';
-import { ClarityModule } from "@clr/angular";
+import {
+  ClrDatagridModule,
+  ClrDropdownModule,
+  ClrIconModule,
+  ClrMainContainerModule,
+  ClrNavigationModule
+} from '@clr/angular';
+import { CompaniesComponent } from './companies/companies.component';
+import { LoginComponent } from './login/login.component';
 
 
 // @ts-ignore
@@ -26,14 +33,22 @@ import { ClarityModule } from "@clr/angular";
     FormsComponent,
     LogoutComponent,
     FooterComponent,
+    CompaniesComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ClarityModule
+    ClrIconModule,
+    ClrMainContainerModule,
+    ClrNavigationModule,
+    ClrDropdownModule,
+    ClrDatagridModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
