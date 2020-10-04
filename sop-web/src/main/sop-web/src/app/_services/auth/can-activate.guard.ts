2a867d134e7e28b7798e11b30316531e1fbf52d1
Kacper Rzymkiewicz <kacper9800@gmail.com>
2020-10-04 17:23:07 +0200

    Can activate guard

---
+++ sop-web/src/main/sop-web/src/app/_services/auth/can-activate.guard.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {TokenStorageService} from './token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActivateGuard implements CanActivate {
+  constructor(private tokenStorageService: TokenStorageService) {
+  }
+
+
+  public canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.tokenStorageService.getUser() != null;
+  }
+
+}
