e44c5cd2a82235125b9d297d6eea9c3f5f3f6232
Kacper Rzymkiewicz <kacper9800@gmail.com>
2020-11-14 17:26:02 +0100

    User registration improved

--- sop-web/src/main/sop-web/src/app/authentication/registration/registration.component.ts
+++ sop-web/src/main/sop-web/src/app/authentication/registration/registration.component.ts
@@ -1,8 +1,11 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from '../../security/user';
 import {AuthService} from '../../_services/auth/auth.service';
 import {Router} from '@angular/router';
+import {Dictionary} from '../../_model/dictionary.model';
+import {DictionariesService} from '../../_services/dictionaries.service';
+import {TranslateService} from '@ngx-translate/core';
+import {UserRegistration} from "../../_model/user-registration.model";
 
 @Component({
   selector: 'app-registration',
@@ -13,6 +16,8 @@ export class RegistrationComponent implements OnInit {
 
   @Output()
   public closeDialogWithSaveEmitter: EventEmitter<any> = new EventEmitter<any>();
+  public sexTypes: Dictionary[] = [];
+  public academicDegrees: Dictionary[] = [];
   public registrationForm: FormGroup;
   public displayRegistrationDialog = false;
   public isSuccessful = false;
@@ -20,12 +25,14 @@ export class RegistrationComponent implements OnInit {
   public errorMessage = '';
   public blockUI: any;
 
-  private userToRegister: User;
+  private userToRegister: UserRegistration;
   private displayAlert: boolean;
 
   constructor(private formBuilder: FormBuilder,
               private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private translateService: TranslateService,
+              private dictionaryService: DictionariesService) {
   }
 
   public show(): void {
@@ -33,22 +40,61 @@ export class RegistrationComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    this.createForm();
+    this.loadSexTypes();
+    this.loadAcademicDegrees();
+  }
+
+  public createForm() {
     this.registrationForm = this.formBuilder.group({
-      token: new FormControl({value: null, disabled: false}),
-      username: new FormControl({value: null, disabled: false}, Validators.required),
+      token: new FormControl({value: null, disabled: false}, Validators.required),
       firstName: new FormControl({value: null, disabled: false}, Validators.required),
       lastName: new FormControl({value: null, disabled: false}, Validators.required),
+      birthDate: new FormControl({value: null, disabled: false}, Validators.required),
+      email: new FormControl({value: null, disabled: false}, Validators.required),
+      sex: new FormControl({value: null, disabled: false}, Validators.required),
       password: new FormControl({value: null, disabled: false}, Validators.required),
       repeatedPassword: new FormControl({value: null, disabled: false}, Validators.required),
-      birthDate: new FormControl({value: null, disabled: false}),
-      sex: new FormControl({value: null, disabled: false}, Validators.required),
-      academicDegree: new FormControl({value: null, disabled: false}, Validators.required),
-      phone: new FormControl({value: null, disabled: false}, Validators.required),
-      // email: new FormControl({
-      //   value: null,
-      //   disabled: false
-      // }, Validators.compose([Validators.required, Validators.email])),
+      academicDegree: new FormControl({value: null, disabled: false}),
+      phone: new FormControl({value: null, disabled: false}),
     });
+  }
+
+  private loadSexTypes() {
+    this.dictionaryService.getSexTypes().subscribe(
+      (res) => this.onSuccessLoadSexTypes(res),
+      (error) => this.onErrorLoadSexTypes(error)
+    );
+  }
+
+  private onSuccessLoadSexTypes(res) {
+    this.sexTypes = [];
+    this.sexTypes.push({value: null, label: 'chooseSexType'});
+    res.forEach(sexType => this.sexTypes.push({label: sexType.value, value: sexType.value}));
+  }
+
+  private onErrorLoadSexTypes(error: any) {
+    this.sexTypes = [];
+  }
+
+  private loadAcademicDegrees() {
+    this.dictionaryService.getAcademicDegrees().subscribe(
+      (res) => this.onSuccessLoadAcademicDegrees(res),
+      (error) => this.onErrorLoadAcademicDegrees(error)
+    );
+  }
+
+  private onSuccessLoadAcademicDegrees(res) {
+    this.academicDegrees = [];
+    this.academicDegrees.push({value: null, label: 'chooseAcademicDegree'});
+    res.forEach(academicDegree => this.academicDegrees.push({
+      label: academicDegree.value,
+      value: academicDegree.value
+    }));
+  }
+
+  private onErrorLoadAcademicDegrees(error: any) {
+    this.academicDegrees = [];
   }
 
   public onSubmit(): void {
@@ -61,7 +107,6 @@ export class RegistrationComponent implements OnInit {
         this.blockUI = false;
         this.displayRegistrationDialog = false;
         this.router.navigate(['registered-successfully']);
-
       },
       err => {
         this.displayAlert = true;
@@ -72,16 +117,25 @@ export class RegistrationComponent implements OnInit {
   }
 
   private collectUserData(): void {
-    this.userToRegister = new User();
+    this.userToRegister = new UserRegistration();
+    this.userToRegister.token = this.registrationForm.get('token').value;
     this.userToRegister.firstName = this.registrationForm.get('firstName').value;
     this.userToRegister.lastName = this.registrationForm.get('lastName').value;
+    this.userToRegister.birthDate = this.registrationForm.get('birthDate').value;
+    this.userToRegister.email = this.registrationForm.get('email').value;
+    this.userToRegister.sex = this.registrationForm.get('sex').value;
     this.userToRegister.password = this.registrationForm.get('password').value;
-    this.userToRegister.email = this.registrationForm.get('username').value;
-    this.userToRegister.username = this.registrationForm.get('username').value;
-    this.userToRegister.role = ['ROLE_USER', 'ROLE_ADMIN'];
+    // this.userToRegister.role = ['ROLE_USER', 'ROLE_STUDENT'];
   }
 
-  public passwordMatchValidator(g: FormGroup) {
-    return this.registrationForm.get('password').value === g.get('repeatedPassword').value ? null : {'mismatch': true};
+  public isPasswordCorrect(): boolean {
+    const password = this.registrationForm.get('password').value;
+    const repeatedPassword = this.registrationForm.get('repeatedPassword').value;
+    if (password == null || repeatedPassword == null) {
+      return false;
+    }
+    if (password === repeatedPassword) {
+      return true;
+    }
   }
 }
