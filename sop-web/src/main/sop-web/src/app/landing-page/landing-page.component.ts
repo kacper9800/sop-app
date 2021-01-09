b590c808f84eb9c50ae78837497ce712ef182320
Kacper Rzymkiewicz <kacper9800@gmail.com>
2021-01-09 01:54:02 +0100

    Landing page improved

--- sop-web/src/main/sop-web/src/app/landing-page/landing-page.component.ts
+++ sop-web/src/main/sop-web/src/app/landing-page/landing-page.component.ts
@@ -6,7 +6,7 @@ import {
   ViewContainerRef
 } from '@angular/core';
 import {CollegeRegistrationComponent} from '../authentication/college-registration/college-registration.component';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 import {AuthService} from '../_services/auth/auth.service';
 import {TokenStorageService} from '../_services/auth/token-storage.service';
 
@@ -16,7 +16,10 @@ import {TokenStorageService} from '../_services/auth/token-storage.service';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  @ViewChild('collegeRegistrationForm', {read: ViewContainerRef, static: false}) entry: ViewContainerRef;
+  @ViewChild('collegeRegistrationForm', {
+    read: ViewContainerRef,
+    static: false
+  }) entry: ViewContainerRef;
   public componentRef: any;
 
   public roles: string[] = [];
@@ -30,8 +33,44 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  scroll(el: HTMLElement) {
+  public scroll(el: HTMLElement) {
+    document.body.scrollTop = 0;
     el.scrollIntoView();
+    const pos = window.pageYOffset;
+    console.log(pos);
+  }
+
+  public scrollTop(el: HTMLElement) {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      console.log(pos);
+      if (pos > 0) {
+        window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
+  }
+
+  public scrollToSectionTwo(sectionTwo: HTMLElement) {
+    const scrollToSectionTwo = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      console.log(pos);
+      if (pos < 1091.111083984375) {
+        window.scrollTo(0, pos + 20); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToSectionTwo);
+      }
+
+      // if (pos === 0) {
+      //   window.scrollTo(0, pos + 20); // how far to scroll on each step
+      // } else if (pos > 0) {
+      //   window.scroll(0, pos + 20); // how far to scroll on each step
+      // } else if (pos === 1100) {
+      //
+      // }
+    }, 16);
+
   }
 
   public showRegisterCollegeForm(): void {
@@ -47,4 +86,6 @@ export class LandingPageComponent implements OnInit {
   public refresh(): void {
     window.location.reload();
   }
+
+
 }
