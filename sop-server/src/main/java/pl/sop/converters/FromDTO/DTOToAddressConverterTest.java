0c47cb5a13eb5c6e66b7fd5866cc7ade4b759c5a
Maciej Siadkowski <maciej.siadkowski@kipertech.com>
2022-01-06 17:19:11 +0100

    Add junit

---
+++ sop-server/src/main/java/pl/sop/converters/FromDTO/DTOToAddressConverterTest.java
@@ -0,0 +1,18 @@
+package pl.sop.converters.FromDTO;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DTOToAddressConverterTest {
+
+    @Test
+    public void tmp() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void tmp2() {
+        assertEquals(5, 2 + 2);
+    }
+}
