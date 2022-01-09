2a6b91178f10e8437fc26414e75d8a60ff1ff2c0
Maciej Siadkowski <maciej.siadkowski@kipertech.com>
2022-01-09 01:41:26 +0100

    Add junit test

---
+++ sop-server/src/test/java/pl/sop/converters/FromDTO/DTOToAddressConverterTest.java
@@ -0,0 +1,22 @@
+package pl.sop.converters.FromDTO;
+
+import org.junit.Test;
+import pl.sop.dto.AddressDTO;
+import pl.sop.entities.Address;
+
+import static org.junit.Assert.*;
+
+public class DTOToAddressConverterTest {
+
+    private final DTOToAddressConverter dtoToAddressConverter = new DTOToAddressConverter();
+
+    @Test
+    public void testConvert() {
+        AddressDTO dto = new AddressDTO("Kraków", "Podchorążych", "2", "30-084");
+        Address address = dtoToAddressConverter.convert(dto);
+        assertEquals(dto.getCity(), address.getCity());
+        assertEquals(dto.getStreet(), address.getStreet());
+        assertEquals(dto.getNumber(), address.getNumber());
+        assertEquals(dto.getPostCode(), address.getPostCode());
+    }
+}
---
+++ sop-server/src/test/java/pl/sop/converters/FromDTO/DTOToCollegeConverterTest.java
@@ -0,0 +1,24 @@
+package pl.sop.converters.FromDTO;
+
+import org.junit.Test;
+import pl.sop.dto.AddressDTO;
+import pl.sop.dto.CollegeDTO;
+import pl.sop.entities.Address;
+import pl.sop.entities.organizationStructure.College;
+
+import static org.junit.Assert.*;
+
+public class DTOToCollegeConverterTest {
+
+    private final DTOToCollegeConverter dtoToCollegeConverter = new DTOToCollegeConverter();
+
+    @Test
+    public void testConvert() {
+        CollegeDTO dto = new CollegeDTO(1L, "College", true, false);
+        College college = dtoToCollegeConverter.convert(dto);
+        assertEquals(dto.getId(), college.getId());
+        assertEquals(dto.getName(), college.getName());
+        assertEquals(dto.getActive(), college.getActive());
+        assertEquals(dto.getDeleted(), college.getDeleted());
+    }
+}
