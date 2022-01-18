6dc70724af920c358a1f698ed1b13b2f540764ce
Kacper Rzymkiewicz <kacper9800@gmail.com>
2022-01-18 23:03:54 +0100

    Changes in User Not Found Exception

--- sop-server/src/main/java/pl/sop/exception/UserNotFoundException.java
+++ sop-server/src/main/java/pl/sop/exception/UserNotFoundException.java
@@ -7,6 +7,6 @@ package pl.sop.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
-        super("Could not find user: " + id +"!");
+        super("Could not find user: " + id);
     }
 }
